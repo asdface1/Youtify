5fee4b6b7d9ebc35c78a4a02f80b530ff4aadba5
Jakob <jakobols@kth.se>
2017-03-24 15:19:13 +0100

    youtube reducer

--- src/Reducers/YoutubeReducer.js
+++ src/Reducers/YoutubeReducer.js
@@ -4,6 +4,8 @@ const initialState = {
 
 export default function reducer(state=initialState, action) {
   switch (action.type) {
+    case 'RESULT':
+      return {...state, results: action.payload.result};
     default:
       return state;
   }
