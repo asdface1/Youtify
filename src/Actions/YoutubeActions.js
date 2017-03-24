78ec89c6fe069a3069b05ab6db0ed802c42880c2
Viktor Yberg <yberg@kth.se>
2017-03-24 14:23:43 +0100

    First fetch

--- src/Actions/YoutubeActions.js
+++ src/Actions/YoutubeActions.js
@@ -1,8 +1,22 @@
 export function search(value) {
-  return {
-    type: 'SEARCH',
-    payload: {
-      search: value,
-    }
+  return function(dispatcher) {
+    fetch(`https://jsonplaceholder.typicode.com/users`, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(response => {
+        console.log('response', response);
+        /*
+        return {
+          type: 'RESULTS',
+          payload: {
+            response
+          }
+        }
+        */
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
   }
 }
