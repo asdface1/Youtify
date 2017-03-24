1a6c0c757875265ec233a2258ebd8811b163dd40
Viktor Yberg <yberg@kth.se>
2017-03-24 15:07:02 +0100

    Added config file

--- src/Actions/YoutubeActions.js
+++ src/Actions/YoutubeActions.js
@@ -1,6 +1,7 @@
 export function search(value) {
   return function(dispatcher) {
-    fetch(`https://jsonplaceholder.typicode.com/users`, {
+    const params = `?part=snippet&maxResults=10&q=${value}&type=video&fields=items(snippet(channelId%2CchannelTitle%2Cdescription%2CpublishedAt%2Cthumbnails%2Fdefault%2Ctitle))%2CnextPageToken%2CpageInfo%2CprevPageToken%2CtokenPagination&key={YOUR_API_KEY}`;
+    fetch(`https://www.googleapis.com/youtube/v3/search${params}`, {
         method: 'GET'
       })
       .then(response => response.json())
