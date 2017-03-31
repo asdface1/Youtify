2e78ac0622930575ad878d08d43cd42a0d331438
Jakob <jakobols@kth.se>
2017-03-31 14:16:55 +0200

    added to index.js

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
+import app from './AppReducer';
 import user from './UserReducer';
 import video from './VideoReducer';
 import youtube from './YoutubeReducer';
 
 export default combineReducers({
+  app,
   user,
   video,
   youtube
