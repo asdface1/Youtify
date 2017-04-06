67a64293133b6eace5470129a69b458f9fd078d0
Viktor Yberg <yberg@kth.se>
2017-04-06 18:50:25 +0200

    Added loader when results are empty

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -42,41 +42,49 @@ class Results extends React.Component {
   }
 
   render() {
-    return (
-      <div id="Results">
-        <div className="ui inverted divided items" style={{ padding: '20px', color: 'lightgrey' }}>
-          {
-            this.props.items.map((item, i) => {
-              return (
-                <div className="item justify-content-center" key={i} draggable onDoubleClick={() => this.play(item, i)}>
-                  <div className="ui tiny image" onClick={() => this.play(item, i)}>
-                    <img className="ui small image" src={item.snippet.thumbnails.medium.url} />
-                    <div className="overlay"><i className="big play icon"/></div>
-                  </div>
-                  <div className={`middle aligned content ${item.id.videoId === this.props.video.song.id.videoId ? 'active' : ''}`}>
-                    {item.snippet.title}
-                  </div>
-                  <div className="flex align-items-center justify-content-center">
-                    <Dropdown pointing="right" icon="ellipsis horizontal">
-                      <Dropdown.Menu>
-                        <Dropdown.Item text='Add to queue' icon="plus" onClick={() => this.addToQueue(item)} />
-                        <Dropdown.Header icon='list' content='Add to playlist' />
-                        { this.props.user.playlists.map(playlist => {
-                          return (
-                            <Dropdown.Item key={playlist.id} text={playlist.name} icon="plus" className="italic"
-                              onClick={() => this.addToPlaylist(item, playlist)} />
-                          )
-                        }) }
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </div>
-                </div>
-              )
-            })
-          }
+    if (this.props.items.length === 0) {
+      return (
+        <div id="Results" style={{ display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <div className="ui large active centered inline loader"></div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div id="Results">
+          <div className="ui inverted divided items" style={{ padding: '20px', color: 'lightgrey' }}>
+            {
+              this.props.items.map((item, i) => {
+                return (
+                  <div className="item justify-content-center" key={i} draggable onDoubleClick={() => this.play(item, i)}>
+                    <div className="ui tiny image" onClick={() => this.play(item, i)}>
+                      <img className="ui small image" src={item.snippet.thumbnails.medium.url} />
+                      <div className="overlay"><i className="big play icon"/></div>
+                    </div>
+                    <div className={`middle aligned content ${item.id.videoId === this.props.video.song.id.videoId ? 'active' : ''}`}>
+                      {item.snippet.title}
+                    </div>
+                    <div className="flex align-items-center justify-content-center">
+                      <Dropdown pointing="right" icon="ellipsis horizontal">
+                        <Dropdown.Menu>
+                          <Dropdown.Item text='Add to queue' icon="plus" onClick={() => this.addToQueue(item)} />
+                          <Dropdown.Header icon='list' content='Add to playlist' />
+                          { this.props.user.playlists.map(playlist => {
+                            return (
+                              <Dropdown.Item key={playlist.id} text={playlist.name} icon="plus" className="italic"
+                                onClick={() => this.addToPlaylist(item, playlist)} />
+                            )
+                          }) }
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      )
+    }
   }
 }
 
